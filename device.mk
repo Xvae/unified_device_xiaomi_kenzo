77db79254519cc8a7ee912f4ab7fb4d7780ec0f8
33bca <33bca@users.noreply.github.com>
2020-03-28 00:43:02 +0000

    msm8956-common: Adapt ANT+ for CAF
    Signed-off-by: Mihran Thalhath <mihranz7@gmail.com>
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- device.mk
+++ device.mk
@@ -112,11 +112,11 @@ PRODUCT_PACKAGES += \
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService \
-    com.dsi.ant.antradio_library \
+    antradio_app  \
     libantradio
 
-PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
+#PRODUCT_COPY_FILES += \
+#    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
 # Doze mode
 PRODUCT_PACKAGES += \
