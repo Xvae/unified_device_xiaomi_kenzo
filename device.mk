MocaRafee/device_xiaomi_kenzo

kenzo: use Pixel 4 fingerprint to bypass CTS
Signed-off-by: Herobuxx <herobuxx@gmail.com>
Signed-off-by: Mocarafee <harif.rp05@gmail.com>

device.mk
@@ -46,6 +46,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/msm8976-tashalite-snd-card_Button_Jack.kl:system/usr/keylayout/msm8976-tashalite-snd-card_Button_Jack.kl \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:system/usr/keylayout/uinput-fpc.kl
 
+# Pixel identification
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.build.fingerprint=google/flame/flame:10/QQ2A.200501.001.A3/6353761:user/release-key
 
 # Ramdisk
 PRODUCT_PACKAGES += \

Xvae/unified_device_xiaomi_kenzo

device.mk
@@ -582,6 +582,10 @@ PRODUCT_ODM_PROPERTIES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.dpm.feature=0
 
+# Pixel identification
+PRODUCT_PRODUCT_PROPERTIES += \
+ro.build.fingerprint=google/flame/flame:10/QQ2A.200501.001.A3/6353761:user/release-keys
+
 # XiaomiParts
 PRODUCT_PACKAGES += \
     XiaomiParts