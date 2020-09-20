4be59235c768a4c665697243a759ab206ca95e83
mrjarvis698 <dudamajaykumar59@gmail.com>
2020-09-19 19:01:19 -0700

    ANT+:NUKED

--- device.mk
+++ device.mk
@@ -124,8 +124,8 @@ PRODUCT_PACKAGES += \
     com.dsi.ant.antradio_library \
     libantradio
 
-PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
+#PRODUCT_COPY_FILES += \
+#    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
 # Doze mode
 PRODUCT_PACKAGES += \
