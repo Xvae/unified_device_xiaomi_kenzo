c05e718a27fce8b968116f25e21a7bf57dc27e43
trkzmn <trkzmn89@gmail.com>
2020-10-02 07:08:23 +0000

    kenzo: Disable psi monitors * Our kernel doesn't support it

--- device.mk
+++ device.mk
@@ -222,6 +222,16 @@ include $(LOCAL_PATH)/rootdir/charger/charger.mk
 PRODUCT_PACKAGES += \
     librmnetctl
 
+# Disable setupwizard
+ifeq ($(DISABLE_SETUPWIZARD),true)
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.setupwizard.mode=DISABLED
+endif
+
+# Disable psi monitors
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.lmk.use_psi=false
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
