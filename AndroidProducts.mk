00ed382f5776e3c40200919d05d06e25c4d21fe3
mocarafee <harif.rp05@gmail.com>
2020-10-17 12:57:55 +0000

    oops 👀

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_kenzo.mk
+    $(LOCAL_DIR)/conquer_kenzo.mk
 
 COMMON_LUNCH_CHOICES := \
     conquer_kenzo-userdebug \
